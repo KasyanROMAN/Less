ba93d748b6299d42637b33e0bec2532073e1b492
Roman Kasyan <kasanr223@gmail.com>
2023-09-15 21:18:46 +0300

    add

---
+++ index.js
@@ -0,0 +1,55 @@
+/*let a = "MISHADANIILVADIM"
+
+console.log(a.length)
+
+
+let b = 22
+
+console.log(typeof b)
+
+let s = "Hello"
+
+console.log(typeof s)
+
+let m = 183817346314781473831487314111111111111111111111111111111111111
+
+console.log(typeof m)
+
+
+let a = null
+console.log(typeof a)
+
+let i 
+
+console.log(typeof i)
+
+
+let a = true
+let b = false
+
+console.log(a)
+console.log(b)
+*/
+
+//<,>,==,!=,<=,=>
+
+//let a = 5 <= 5
+
+//console.log(a)
+
+//let a = 22
+
+//let b = 33
+
+//let value = a == b
+
+//console.log(value)
+
+let a = 33
+let b = 22
+
+let v = a < b
+
+console.log(v)
+
+
